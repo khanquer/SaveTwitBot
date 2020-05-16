ab42b3716ca9f0aa2bf506a92852e1d03e51708f
khanquer <shafar0@gmail.com>
2020-05-16 11:28:12 -0400

    fonts and stuff

--- main.py
+++ main.py
@@ -18,20 +18,17 @@ testing = True
 
 def sendPic(api, tweet, num):
 	res = api.media_upload(base_path + '/media/sendfinal{}.jpg'.format(num))
-	api.update_status('@' + tweet.user.screen_name, media_ids=[res.media_id], in_reply_to_status_id=tweet.id)
+	api.update_status(media_ids=[res.media_id], in_reply_to_status_id=tweet.id)
 
 
 def give_emoji_free_text(text):
-    allchars = [str for str in text]
-    emoji_list = [c for c in allchars if c in emoji.UNICODE_EMOJI]
-    clean_text = ' '.join([str for str in text.split() if not any(i in str for i in emoji_list)])
-
-    return clean_text
+	clean_text = emoji.get_emoji_regexp().sub(u'', text)
+	return clean_text
 
 def formatText(text):
 	text = give_emoji_free_text(text)
 	finaltext = ''
-	maxlen = 30
+	maxlen = 26
 	lines = text.split('\n')
 	# print(len(lines))
 	for line in lines:
@@ -60,6 +57,7 @@ def formatText(text):
 def genTweetPic(api, origTweetId, num):
 	origTweet = api.get_status(origTweetId, tweet_mode='extended')
 	tweetdict = origTweet.__dict__
+	print('KEYS FOR TWEET/status')
 	print(tweetdict.keys())
 	# print(tweetdict['entities'])
 	print(tweetdict['user'].profile_image_url)
@@ -81,7 +79,7 @@ def genTweetPic(api, origTweetId, num):
 
 	nsize = 800
 
-	#print(origTweet.full_text)
+	print(origTweet.full_text)
 	print(type(userA.name))
 	print(repr(userA.name))
 	print(userA.name.encode('utf-8'))
@@ -95,7 +93,7 @@ def genTweetPic(api, origTweetId, num):
 	d = ImageDraw.Draw(bg_img)
 
 	fnt2 = ImageFont.truetype(base_path + '/fonts/OpenSansEmoji.ttf', size=nsize/21)
-	fnt3 = ImageFont.truetype(base_path + '/fonts/Roboto-Regular.ttf', size=nsize/23)
+	fnt3 = ImageFont.truetype(base_path + '/fonts/Roboto-Bold.ttf', size=nsize/23)
 	d.text((nsize/4, nsize/16), userA.name, font=fnt2, fill=(255, 255, 255))
 	d.text((nsize/4, nsize/7.7), '@' + userA.screen_name, font=fnt3, fill=(255, 255, 255))
 
@@ -103,13 +101,13 @@ def genTweetPic(api, origTweetId, num):
 	prof_img = prof_img.resize((nsize/8,nsize/8))
 	bg_img.paste(prof_img, (nsize/16, nsize/16))
 
-	fnt1 = ImageFont.truetype(base_path + '/fonts/OpenSansEmoji.ttf', size=nsize/17)
+	fnt1 = ImageFont.truetype(base_path + '/fonts/Roboto-Regular.ttf', size=nsize/17)
 	#d.text((nsize/16, nsize/4), finaltext, font=fnt1, fill=(255, 255, 255))
 	d.text((nsize/16, nsize/4), finaltext, font=fnt1)
 
 	fnt4 = ImageFont.truetype(base_path + '/fonts/Roboto-Regular.ttf', size=nsize/25)
 	dttime = str(created_dttm.strftime("%-I:%M %p   %d %b %y"))
-	d.text((nsize/16, hsize - nsize/10), dttime, font=fnt4, fill=(255, 255, 255))
+	d.text((nsize/16, hsize - nsize/10), dttime, font=fnt4, fill=(169,169,169))
 
 	bg_img.save(base_path + '/media/sendfinal{}.jpg'.format(num))
 	pass
@@ -146,10 +144,15 @@ def main():
 
 	num = 0
 	for tweet in reversed(tweets):
-		print(str(tweet.id))# + ' -- ' + tweet.text + ' -- REPLYING TO ' + str(tweet.in_reply_to_status_id))
+		
+		print(str(tweet.id) + ' -- ' + tweet.text + ' -- REPLYING TO ' + str(tweet.in_reply_to_status_id))
 		if(tweet.id <= last_replied_id):
 			continue
+		if(tweet.text[:13] == '@SaveTweetBot'):
+			continue
+		#print(tweet)
 		origTweetId = tweet.in_reply_to_status_id
+		print('in reply ' + str(tweet.in_reply_to_status_id))
 
 		if origTweetId:
 			genTweetPic(api,origTweetId,num)
--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source ~/Projects/SaveTwitBot/venv/bin/activate
+#pip install -r ~/Projects/SaveTwitBot/requirement.txt
 python main.py
