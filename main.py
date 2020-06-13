64f974ea8586cb724baf8b2bf9e35c0bac073fcc
khanquer <shafar0@gmail.com>
2020-06-13 15:59:07 -0400

    sub stuff

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 import urllib
 import emoji
+import os
+#print(os.path.abspath(os.getcwd()))
 	
 consumer_key = 'TeBR4DGeH6NsSKrdFvgrwYJva'
 consumer_secret = 'YqjvbJZKhgqXRDl6VxurwGxsLfBzLEByZGLcSKjxYSifS1LIJe'
@@ -12,17 +14,26 @@ access_token_secret = 'nJsTlLBaBYGJtpl3CHHY9sSC6UnwFvifiehxTUwSIm6Yl'
 global base_path
 #base_path = '/home/imrhankhan_shajahan/Projects/SaveTweetBot'
 base_path = '/home/imri_linux/Projects/SaveTwitBot'
+#base_path = os.path.abspath(os.getcwd())
+#print(base_path)
 
 global testing
 testing = True
 
 def sendPic(api, tweet, num):
 	res = api.media_upload(base_path + '/media/sendfinal{}.jpg'.format(num))
-	api.update_status(media_ids=[res.media_id], in_reply_to_status_id=tweet.id)
+	api.update_status('@' + tweet.user.screen_name, media_ids=[res.media_id], in_reply_to_status_id=tweet.id)
 
 
 def give_emoji_free_text(text):
 	clean_text = emoji.get_emoji_regexp().sub(u'', text)
+	print(clean_text)
+	txt_list = (clean_text.split(' '))
+	for i in txt_list:
+		#print('https://t.co' in i)
+		if ('https://t.co' in i):
+			txt_list.remove(i)
+	clean_text = ' '.join(txt_list)
 	return clean_text
 
 def formatText(text):
@@ -53,23 +64,12 @@ def formatText(text):
 	return (finaltext,numlines)
 
 
-
-def genTweetPic(api, origTweetId, num):
-	origTweet = api.get_status(origTweetId, tweet_mode='extended')
+def obtainProfPic(origTweet,num):
 	tweetdict = origTweet.__dict__
-	print('KEYS FOR TWEET/status')
-	print(tweetdict.keys())
-	# print(tweetdict['entities'])
-	print(tweetdict['user'].profile_image_url)
-	print(tweetdict['created_at'])
-	#dttmobj = datetime.strptime(tweetdict['created_at'], '%Y-%m-%d %H:%M:%S')
-	created_dttm = tweetdict['created_at']
-	print(created_dttm.strftime("%-I:%M %p   %d %b %y"))
-	#print(created_dttm.year)
-
 	userA = tweetdict['user']
-	print(type(userA))
-	print(userA.__dict__.keys())
+	#print(type(userA))
+	#print(userA.__dict__.keys())
+	#PROFILE PICTURE
 	img_url = tweetdict['user'].profile_image_url
 	img_local_path = base_path + '/media/profpic{}.jpg'.format(num)
 	try:
@@ -77,17 +77,50 @@ def genTweetPic(api, origTweetId, num):
 	except:
 		pass
 
-	nsize = 800
+def obtainTweetPics(origTweet,num):
+	if(u'media' in origTweet.entities.keys()):
+		tw_img = True
+		print(origTweet.entities['media'])
+		print(origTweet.entities['media'][0])
+		print(origTweet.entities['media'][0]['media_url'])
+		img_url = origTweet.entities['media'][0]['media_url'] 
+		img_local_path = base_path + '/media/tweetpic{}.jpg'.format(num)
+		try:
+			urllib.urlretrieve(img_url, img_local_path)
+			return True
+		except:
+			return False
 
-	print(origTweet.full_text)
-	print(type(userA.name))
-	print(repr(userA.name))
-	print(userA.name.encode('utf-8'))
-	#print(userA.encode('ascii', 'ignore').decode('ascii'))
+def genTweetPic(api, origTweetId, num):
+	origTweet = api.get_status(origTweetId, tweet_mode='extended')
 
-	finaltext,numlines = formatText(origTweet.full_text)
+
+	print('ORIGINAL TWEET ENTITIES')
+	img_urls = None
+	tw_img = False
+	print('\n \n \n')
+		
+	print('KEYS FOR TWEET/status')
+	tweetdict = origTweet.__dict__
+	userA = tweetdict['user']
+
+	print(tweetdict['created_at'])
+	created_dttm = tweetdict['created_at']
+
+
+	obtainProfPic(origTweet, num)
+	tw_img = obtainTweetPics(origTweet, num)
 	
-	hsize = int(nsize/2) + numlines*45
+	finaltext,numlines = formatText(origTweet.full_text)
+
+	#if (tw_img):
+
+	nsize = 800
+	
+	if (tw_img is True):
+		hsize = int(nsize/2) + numlines*45 + int(nsize/2)
+	else:
+		hsize = int(nsize/2) + numlines*45
 
 	bg_img = Image.new('RGB', (nsize, hsize), color=(0, 0, 0))
 	d = ImageDraw.Draw(bg_img)
@@ -109,9 +142,36 @@ def genTweetPic(api, origTweetId, num):
 	dttime = str(created_dttm.strftime("%-I:%M %p   %d %b %y"))
 	d.text((nsize/16, hsize - nsize/10), dttime, font=fnt4, fill=(169,169,169))
 
-	bg_img.save(base_path + '/media/sendfinal{}.jpg'.format(num))
-	pass
+	if (tw_img):
+		pic_width = 700
+		pic_height = 400
+		img_url = Image.open(base_path + '/media/tweetpic{}.jpg'.format(num))
+		print(img_url.size)
+		img_width,img_height = img_url.size
+		print(pic_width/float(img_width))
+		pic_size = (pic_width, int(pic_width/float(img_width) * img_height))
+		print(pic_size)
+		img_url = img_url.resize(pic_size)
+		print(img_url.size)
+		img_width,img_height = img_url.size
 
+		left = 0
+		#top = int((img_height - pic_height)/2)
+		top = 0
+		right = pic_size[0]
+		#bottom = img_height - int((img_height - pic_height)/2)
+		bottom = pic_height
+
+		print(left,top,right,bottom)
+		img_url = img_url.crop((left,top,right,bottom))
+		print(img_url.size)
+		#img_url.show()
+		print(img_url.size)
+		bg_img.paste(img_url, (nsize/16, int(nsize/16) + 180 + numlines*50))
+
+	bg_img.save(base_path + '/media/sendfinal{}.jpg'.format(num))
+	print('HI')
+	pass
 
 
 def write_last_replied(num):
@@ -146,17 +206,22 @@ def main():
 	for tweet in reversed(tweets):
 		
 		print(str(tweet.id) + ' -- ' + tweet.text + ' -- REPLYING TO ' + str(tweet.in_reply_to_status_id))
+		print(tweet.text[:13])
 		if(tweet.id <= last_replied_id):
 			continue
 		if(tweet.text[:13] == '@SaveTweetBot'):
 			continue
-		#print(tweet)
+
 		origTweetId = tweet.in_reply_to_status_id
-		print('in reply ' + str(tweet.in_reply_to_status_id))
+		#print('in reply ' + str(tweet.in_reply_to_status_id))
 
 		if origTweetId:
-			genTweetPic(api,origTweetId,num)
-			print(tweet.id)
+			try:
+				genTweetPic(api,origTweetId,num)
+			except Exception as ex:
+				print(str(ex))
+				continue
+			#print(tweet.id)
 			if (not testing):
 				sendPic(api,tweet,num)
 				write_last_replied(tweet.id)
